fa9b833dae7a470e5cafbc7100d026b6a5ef1d59
Sayantan Khan <bolt.khan@gmail.com>
2015-12-27 18:30:23 +0530

    Moving screen

---
+++ src/StateManager.hs
@@ -0,0 +1,16 @@
+module StateManager where
+
+import Data.IORef
+import DataTypes
+import Control.Monad.Error
+
+type Env = IORef [(String, IORef LispVal)]
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+type IOThrowsError = ErrorT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
